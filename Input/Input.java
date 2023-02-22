1ffefd7c586e7a4198fe05fa23df941d620b8f3f
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-02-22 15:32:35 +0100

    add input folder

---
+++ Input/Input.java
@@ -0,0 +1,17 @@
+
+// Import java.util.Scanner
+// create scanner object
+// make some questions
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What's your name?");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+    }
+}
