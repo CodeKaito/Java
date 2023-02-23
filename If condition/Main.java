9644da6936424a9d8f9a05d26ebe2caa33fbb457
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-02-23 10:20:51 +0100

    if

--- If condition/Main.java
+++ If condition/Main.java
@@ -1,11 +1,16 @@
 public class Main {
     public static void main(String[] args){
         boolean isOnline = false;
-
+        /* 
         if(isOnline){
             System.out.println("è online");
         } else {
             System.out.println("non è online");
         }
+        */
+
+        String result = (isOnline == true) ? "è online" : "non è online";
+
+        System.out.println(result);
     }    
 }
