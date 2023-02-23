ed9a33a821d377a5d4794540abe0a3d6ee08d72a
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-02-23 10:07:28 +0100

    Update Main.java

---
+++ If condition/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        boolean isOnline = true;
+
+        if(isOnline){
+            System.out.println("è online");
+        } else {
+            System.out.println("non è online");
+        }
+    }    
+}
