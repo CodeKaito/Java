cf6f7bfcf394f66fe8f9dac2aac2fa51e1d574a5
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-02-23 09:38:26 +0100

    add logic

---
+++ Logic/Logic.java
@@ -0,0 +1,8 @@
+// Logic operators: and && or || not !
+
+public class Logic {
+    public static void main(String[] args){
+        boolean x = 3 < 10 && 3 % 2 == 0;
+        System.out.println(x);
+    }    
+}
