57b517b1ea153d62a56765dfa64762ff1fe57097
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-02-24 14:51:23 +0100

    add for

---
+++ Cicli/For/Main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args){
+        //do a for loop
+        for(int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
+    }
+}
