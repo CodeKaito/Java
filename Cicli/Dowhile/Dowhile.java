9f0f5243f292efe476a09a839953447414dbb3ff
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-02-24 14:12:10 +0100

    Update Dowhile.java

--- Cicli/Dowhile/Dowhile.java
+++ Cicli/Dowhile/Dowhile.java
@@ -2,7 +2,7 @@ public class Dowhile {
     public static void main(String[] args)
     {
         int i = 0;
-        while (i<5)
+        while (i < 5)
         {
             System.out.println(i);
             i++;
