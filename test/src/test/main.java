b8b9679a0de64f4df192a42c269ded6f256b6cc3
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-02-28 18:44:24 +0100

    Update main.java

--- test/src/test/main.java
+++ test/src/test/main.java
@@ -1,9 +1,14 @@
 package test;
 
+/**
+ * @author ADMMCSIDecinco
+ *
+ */
 public class main {
 	public static void main(String[] args) {
 		Persona persona1 = new Persona();
-
-        System.out.println(persona1.colorePreferito);
+		
+        System.out.println(persona1.cognome);
+        persona1.cammina();
 	}	
 }
